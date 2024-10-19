6b137acaf9ed64a9e8e940af76b3cff0d3004001
Nick <nikngeno@gmail.com>
2024-10-19 15:33:26 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExtraCredit1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 6; i >= 1; i--)
+            {
+                for (int j = i; j >= 1; j--)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
